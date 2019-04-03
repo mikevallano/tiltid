fc487cef51226edf6dd3975582459f7951950bce
mike vallano <mikevallano@gmail.com>
2019-04-03 10:56:24 -0500

    Add pagination using will_paginate gem. Close #3.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+test_user = User.find_by(email: 'testmike@example.com')
+unless test_user.present?
+  User.create(email: 'testmike@example.com', password: 'Password8')
+end
+
+3.times do
+  user = User.create!(email: Faker::Internet.email, password: SecureRandom.hex(10))
+  puts "user created. id: #{user.id}"
+end
+
+100.times do |i|
+  body = "#{Faker::TvShows::Seinfeld.quote}\n\n#{Faker::Markdown.random}"
+  til_log = TilLog.create(title: i.even? ? Faker::ChuckNorris.fact : Faker::TvShows::DumbAndDumber.quote,
+                          body: body,
+                          user: i < 30 ? User.first : User.all.sample,
+                          public: [true, false].sample,
+                          tag_list: i.even? ? ['chuck'] : ['dumb'])
+  puts "til_log created: #{til_log.id}"
+end
